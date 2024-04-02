c311719a60a5c7667eab3f7a8456651567c7a115
buidinhhuy <buidinhhuy900@gmail.com>
2024-04-02 10:31:44 +0700

    fix the adding style

--- src/app/container/adding/adding.component.ts
+++ src/app/container/adding/adding.component.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import { NgIf } from '@angular/common';
+import { Component, } from '@angular/core';
 
 @Component({
   selector: 'app-adding',
   standalone: true,
-  imports: [],
+  imports: [NgIf],
   templateUrl: './adding.component.html',
   styleUrl: './adding.component.scss'
 })
 export class AddingComponent {
-
+  isAddClick :boolean = false
+  adding()
+  {
+    this.isAddClick = true
+  }
+  outAdding()
+  {
+    this.isAddClick = false;
+  }
 }
