a6c7c9cfeaf6fcef6de4db08db8cf923f063e309
VTS\huybuidinh <aloalo123>
2024-04-23 12:00:27 +0700

    MyProj

---
+++ MyProj/Qlvb/qlvb.client/src/app/app.component.ts
@@ -0,0 +1,21 @@
+import { HttpClient } from '@angular/common/http';
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+
+export class AppComponent {
+
+  constructor(private http: HttpClient) {}
+
+  // ngOnInit() {
+  // }
+
+
+
+  title = 'qlvb.client';
+}
---
+++ MyProj/Qlvb/qlvb.client/src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { NavbarComponent } from './navbar/navbar.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    NavbarComponent
+  ],
+  imports: [
+    BrowserModule, HttpClientModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
